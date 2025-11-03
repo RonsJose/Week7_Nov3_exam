25f59cc9ad69c00246647bd8642308444ad4c32a
RonsJose-STUDENT <g00464215@atu.ie>
2025-11-03 09:37:24 +0000

    Added add feature

--- src/main/java/ie/atu/main.java
+++ src/main/java/ie/atu/main.java
@@ -8,22 +8,54 @@ public class main {
         Scanner sc = new Scanner(System.in);
         ArrayList<Customer> customers = new ArrayList<Customer>();
         int option=0;
+        Customer customer;
+        String id;
+        String name;
+        String email;
+        String phone;
 
         while(option!=4)
         {
             System.out.println("==== Customer Management System ====\n\n1) Add Customer\n\n2) Update Customer\n\n 3) Display All\n\n 4) Exit\n\n");
 
-            System.out.print("Enter: ");
-            String optionStr=sc.nextLine().trim();
+           while(true)
+           {
+               option=0;
+               System.out.print("Enter number: ");
+               String optionStr=sc.nextLine().trim();
 
-            try
-            {
-                option=Integer.parseInt(optionStr);
-            }
-            catch(NumberFormatException e)
-            {
-                System.out.println("Please enter a number: " + e.getMessage() );
-            }
+               try
+               {
+                   option=Integer.parseInt(optionStr);
+                   break;
+               }
+               catch(NumberFormatException e)
+               {
+                   System.out.println("Please enter a number: " + e.getMessage() );
+               }
+           }
+           customer = new Customer();
+           switch(option)
+               {
+               case 1:
+                   System.out.println("Enter Customer ID(5 digits): ");
+                   id=sc.nextLine().trim();
+
+                   for(Customer c : customers)
+                   {
+                       while (id.equals(c.getCustomerId()))
+                       {
+                           System.out.println("Customer ID is already in use");
+                           System.out.println("Please enter another id: ");
+                           id=sc.nextLine().trim();
+                       }
+                   }
+                   customer.setCustomerId(id);
+                   customers.add(customer);
+                   break;
+               }
+
+
         }
     }
 }
